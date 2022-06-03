696ed4fc58c8a65b9eb914340016699ed4f82b39
lybmvvs <stik9543@gmail.com>
2022-06-03 19:11:21 +0300

    test red

--- Django_try/lybmvvsapp/tests/test_logic.py
+++ Django_try/lybmvvsapp/tests/test_logic.py
@@ -2,7 +2,7 @@ import requests
 from django.test import TestCase, Client
 from django.urls import reverse
 
-
+client = Client()
 class TestViews(TestCase):
     def test_create(self):
         response = self.client.get(reverse('create'))
