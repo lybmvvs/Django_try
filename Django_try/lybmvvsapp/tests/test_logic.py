d73b13e3fb1f313850f7e7c33eba794efd90040c
lybmvvs <stik9543@gmail.com>
2022-06-03 19:09:25 +0300

    test added

---
+++ Django_try/lybmvvsapp/tests/test_logic.py
@@ -0,0 +1,9 @@
+import requests
+from django.test import TestCase, Client
+from django.urls import reverse
+
+
+class TestViews(TestCase):
+    def test_create(self):
+        response = self.client.get(reverse('create'))
+        self.assertEqual(response.status_code, 200)
